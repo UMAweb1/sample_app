5eaf3e604e5f1284485dc52b960c007fd1484d0c
yuma-oji <yumhouse.a@gmail.com>
2019-10-02 15:27:50 +0900

    [add2]sample_app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_104038) do
+ActiveRecord::Schema.define(version: 2019_10_01_105959) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
