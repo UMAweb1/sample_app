e2db42c16f44d181ae04f38efb9ba7981210eb2e
yuma-oji <yumhouse.a@gmail.com>
2019-10-03 16:36:14 +0900

    [add5]sample_app

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -32,9 +32,15 @@ class TodolistsController < ApplicationController
    		redirect_to todolist_path(list.id)
    	end
 
+   	def destroy
+   		list = List.find(params[:id])  #データ(レコード)を1件取得
+   		list.destroy #データ(レコード)を削除
+   		redirect_to todolists_path #List一覧画面蹴へリダイレクト
+   	end
+
    private
 
    def list_params
-        params.require(:list).permit(:title, :body)
+        params.require(:list).permit(:title, :body, :image)
    end
 end
